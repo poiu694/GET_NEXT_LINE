de6c2d9f44ab4fd06df0cf19efa5cdc7d853d9eb
Lee <43488305+poiu694@users.noreply.github.com>
2021-12-23 12:57:57 +0900

    feat: add prototype header for GNL

---
+++ get_next_line.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sangmlee <sangmlee@student.42seoul.kr>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/23 11:40:17 by sangmlee          #+#    #+#             */
+/*   Updated: 2021/12/23 12:53:55 by sangmlee         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+char	*get_next_line(int fd);
+
+size_t	ft_strlen(const char *s);
+char	*ft_strdup(const char *s);
+size_t	ft_strlcpy(char *dest, const char *src, size_t dstsize);
+char	*ft_strjoin(char const *s1, char const *s2);
+
+#endif
