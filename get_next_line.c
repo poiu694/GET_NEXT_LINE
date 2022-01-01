9b7995146c99067204060e346ae26c86677cda8f
Lee <43488305+poiu694@users.noreply.github.com>
2022-01-01 13:24:55 +0900

    [Chore] Complete mandatory get next line

--- get_next_line.c
+++ get_next_line.c
@@ -6,31 +6,11 @@
 /*   By: sangmlee <sangmlee@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/23 11:36:18 by sangmlee          #+#    #+#             */
-/*   Updated: 2021/12/29 13:28:07 by sangmlee         ###   ########.fr       */
+/*   Updated: 2022/01/01 13:22:50 by sangmlee         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
-static char	*ft_read_new_line(int fd, char **cache_storage)
-{
-	char	*read_buf;
-	ssize_t	read_size;
-
-	while (1)
-	{
-		read_size = read(fd, read_buf, BUFFER_SIZE);
-		if (read_size < 0)
-		{
-			free(read_buf);
-			cache_storage = NULL;
-			return (NULL);
-		}
-		read_buf[read_size] = '\0';
-		cache_storage[fd] = ft_strjoin(cache_storage[fd], read_buf);
-	}
-	return (cache_storage[fd]);
-}
 
 static char	*find_newline_index(char *buf)
 {
@@ -39,53 +19,76 @@ static char	*find_newline_index(char *buf)
 	return (buf);
 }
 
+static char	*ft_read_new_line(int fd, char *cache)
+{
+	char	*read_buf;
+	char	*temp;
+	ssize_t	read_size;
+
+	read_buf = malloc(sizeof(char) * (BUFFER_SIZE + 1));
+	if (read_buf == NULL)
+		return (NULL);
+	read_size = read(fd, read_buf, BUFFER_SIZE);
+	while (read_size > 0)
+	{
+		read_buf[read_size] = '\0';
+		temp = cache;
+		cache = ft_strjoin(cache, read_buf);
+		free(temp);
+		if (*find_newline_index(cache) == '\n')
+			break ;
+		read_size = read(fd, read_buf, BUFFER_SIZE);
+	}
+	free(read_buf);
+	if (read_size < 0)
+		return (NULL);
+	return (cache);
+}
+
 static char	*ft_get_line_before_newline(char *buf)
 {
 	char	*line;
 	char	*newline_index;
 
-	newline_index = find_newline(buf);
-	if (*buf == '\0' || *newline_index == '\0')
+	if (*buf == '\0')
 		return (NULL);
-	line = malloc(sizeof(char) * (newline_index - buf + 1));
+	newline_index = find_newline_index(buf);
+	if (*newline_index == '\0')
+		return (ft_strdup(buf));
+	line = malloc(sizeof(char) * (newline_index - buf + 2));
 	if (line == NULL)
 		return (NULL);
-	ft_strlcpy(line, buf, newline_index - buf + 1);
+	ft_strlcpy(line, buf, newline_index - buf + 2);
 	return (line);
 }
 
-static char	*ft_update_cache(char *buf)
+static char	*ft_update_cache(char *cache)
 {
 	char	*line;
 	char	*newline_index;
 
-	newline_index = find_newline(buf);
-	if (newline_index == '\0')
+	newline_index = find_newline_index(cache);
+	if (*newline_index == '\0')
 	{
-		free(buf);
+		free(cache);
 		return (NULL);
 	}
 	line = ft_strdup(newline_index + 1);
+	free(cache);
 	return (line);
 }
 
 char	*get_next_line(int fd)
 {
-	int			error;
-	char		*read_buf;
-	static char	*cache_storage[13000];
+	char		*line;
+	static char	*cache;
 
-	if (fd < 0 || fd >= 13000 || BUFFER_SIZE < 1)
+	if (fd < 0 || BUFFER_SIZE < 1)
 		return (NULL);
-	read_buf = NULL;
-	if (cache_storage[fd] == NULL)
-		cache_storage[fd] = ft_strdup("");
-	read_buf = ft_read_new_line(fd, &cache_storage[fd]);
-	if (read_buf)
-	{
-		read_buf = ft_get_line_before_newline(read_buf);
-		if (read_buf)
-			cache_storage[fd] = ft_update_cache(cache_storage[fd]);
-	}
-	return (read_buf);
+	cache = ft_read_new_line(fd, cache);
+	if (cache == NULL)
+		return (NULL);
+	line = ft_get_line_before_newline(cache);
+	cache = ft_update_cache(cache);
+	return (line);
 }
